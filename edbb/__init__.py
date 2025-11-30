5cfe01f8333e25a0484f0a7b22ab8f7fba31b6ff
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2025-11-30 12:14:15 +0900

    register関数の追加

--- edbb/__init__.py
+++ edbb/__init__.py
@@ -2,4 +2,8 @@
 
 from .initializer import __version__
 from .error_handling import print_error_message
+from .register import register
 
+__all__ = [
+    '__version__', 'print_error_message', 'register'
+]
---
+++ edbb/register.py
@@ -0,0 +1,4 @@
+# EDBBで自動生成するデータに関する詳細情報を登録するためのモジュール
+def register(*args, **kwargs):
+    # 現時点では何もしない
+    pass
