7ba57f931f8d04803c0895e9c50708e57f59b30b
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2025-11-30 20:52:00 +0900

    パッチを入れる

--- edbb/register.py
+++ edbb/register.py
@@ -1,4 +1,11 @@
+from discord.ext import commands
+
 # EDBBで自動生成するデータに関する詳細情報を登録するためのモジュール
 def register(*args, **kwargs):
     # 現時点では何もしない
     pass
+
+def patch_bot(bot: commands.Bot):
+    # 将来的にbotにパッチを当てるための関数
+    pass
+
